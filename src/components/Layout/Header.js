72afa63c1b7edff2245804e58a1b726262077ea6
Kordian <kordian.kolasa@gmail.com>
2021-06-24 20:59:31 +0200

    Added header section

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,16 @@
+import {Fragment} from "react";
+import classes from "./Header.module.css";
+import foodImg from "../../assets/food-unsplash.jpg";
+
+const Header =(props)=>{
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>Food Ordering App</h1>
+            {/* <button>Cart</button> */}
+        </header>
+        <div className={classes['main-image']}>
+            <img src={foodImg} alt="A table with food"/>
+        </div>
+    </Fragment>
+};
+export default Header;
